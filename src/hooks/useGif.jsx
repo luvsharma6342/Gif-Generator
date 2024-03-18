a72b2d083ac18c50c292ef7ec82e9b1218eb7e41
Luv Sharma <luvsharma105@gmail.com>
2024-03-18 10:45:38 +0530

    first commit

---
+++ src/hooks/useGif.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+
+
+function useGif(tag) {
+    const [gif, setGif] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    async function fetchData(tag = null) {
+        setLoading(true);
+        const { data } = await axios.get(tag ? `${url}&tag=${tag}` : url);
+        const imageSource = data.data.images.downsized_large.url;
+        setGif(imageSource);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        if (tag === null) {
+            fetchData();
+        }
+        else {
+            fetchData(tag);
+        }
+    }, [])
+
+    return { gif, loading, fetchData };
+
+}
+
+export default useGif
